f5755d3a3ee398d386678951f6f4cc8296eb1baa
Mark Keys <markwk36@comcast.net>
2020-11-10 17:53:00 +0000

    learning about dictionaries

--- dict01/pythondict01.py
+++ dict01/pythondict01.py
@@ -8,12 +8,38 @@ print( switch["hostname"] )
 print( switch["ip"] )
 
 ## request a 'fake' key
-print( switch["hostname"] )  
+# print( switch["lynx"] )
 
+## request a 'fake' key with .get() method
+print( "First test - .get()" )
+print( switch.get("lynx") )
 
-list1 = [{'id': '642459', 'name': 'Mark Keys', 'url': 'https://n218.meraki.com/o/YO9F3c/manage/organization/overview'}]
+print( "Second test - .get()" )
+print( switch.get("lynx", "THE KEY IS IN ANOTHER CASTLE!") )
 
-print(list1[0]["name"])
+print( "Third test - .get()" )
+print( switch.get("version") )
+
+print( "Fourth test - .keys()" )
+print( switch.keys() )
+
+print( "Fifth test - .values()" )
+print( switch.values() )
+
+print( "Sixth test - .pop()" )
+switch.pop("version") # removes this key (and value) pair
+print( switch.keys() )   # notice the value of version is gone
+print( switch.values() ) # notice the value 1.2
+
+print( "Seventh test - ADD a new value" )
+switch["adminlogin"] = "karl08"
+print( switch.keys() )
+print( switch.values() )
+
+print( "Eighth test - ADD a new value" )
+switch["password"] = "qwerty"
+print( switch.keys() )
+print( switch.values() )
 
 
 
